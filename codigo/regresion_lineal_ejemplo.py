c9025829c8eb7c22d7e42e3e41a12ea21215c9be
Alfredo Olguin <alfredini31@hotmail.com>
2024-09-03 14:23:41 -0600

    Agregamos graficos de las variables x y
    Prueba 1

--- codigo/regresion_lineal_ejemplo.py
+++ codigo/regresion_lineal_ejemplo.py
@@ -8,17 +8,18 @@ Created on Thu Aug 22 00:04:11 2024
 
 ## Regresión ejemplo
 
-from sklearn import linear_model
+from sklearn import linear_model # Esto carga la libreria del MCO
+import matplotlib.pyplot as plt 
 
-reg = linear_model.LinearRegression()
+reg = linear_model.LinearRegression() # Asigno una funcion a reg
 
-x = [[1,2,3,4,5]]
+x = [[1,2,3,4,5]]  # construimos X un data frame
 
-y = [1,2,3,4,5]
+y = [1,2,3,4,5] # construimos y que es un vector
 
-
+# Ajusta la regresion
 reg.fit(x,y)
-reg.coef_
+reg.coef_ # Regresa el coeficiente.
 
 
 ## Regresión completa
@@ -26,19 +27,35 @@ reg.coef_
 import statsmodels.api as sm
 import pandas as pd
 
-df = pd.read_csv('data/Housing.csv', sep=',')
+df = pd.read_csv('data/advertising_training.csv', sep=',')
 
 #define response variable
-y = df['price']
+y = df['Sales']
 
 #define predictor variables
-x = df[['area']]
-
-#add constant to predictor variables
-x = sm.add_constant(x)
+x = df[['TV','Radio', 'Newspaper']]
 
 #fit linear regression model
 model = sm.OLS(y, x).fit()
 
 #view model summary
 print(model.summary())
+
+## Ajustamos para crear un vector "linea"
+y_fit = model.predict(x)
+
+
+## Graficando mi mejor variable predictiva
+a = df['Sales']
+b = df['Radio']
+
+plt.scatter(a, b)
+
+# Agregar etiquetas
+plt.xlabel('Gasto en Radio (miles de USD)')
+plt.ylabel('Numero de ventas')
+plt.title('Grafica de dispersion de Ventas vs Gasto en Radio')
+
+plt.plot(b, y_fit, color='red')
+plt.show()
+

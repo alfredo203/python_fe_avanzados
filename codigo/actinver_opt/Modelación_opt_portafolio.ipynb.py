47ff3c8c835c185a5978f7e781effe1ea2092a6d
Dayjingg <reydavid10910@gmail.com>
2024-10-08 14:58:00 -0600

    modulo de la parte de optimizacion
    se crearon modulos de la parte de la optimizacion pero faltan coregir errores en la parte calculate_opt

--- codigo/actinver_opt/Modelación_opt_portafolio.ipynb.py
+++ codigo/actinver_opt/Modelación_opt_portafolio.ipynb.py
@@ -12,19 +12,33 @@ import matplotlib.pyplot as plt
 
 # Fechas
 start = datetime.datetime(2021, 1, 1)
-end = datetime.datetime(2021, 1, 22)
-
-# Datos de las acciones
-ceva = yf.download("CEVA", start=start, end=end)
-google = yf.download("GOOGL", start=start, end=end)
-tesla = yf.download("TSLA", start=start, end=end)
-zom = yf.download("ZOM", start=start, end=end)
+end = datetime.datetime(2024, 1, 22)
+a = yf.download("OXY1.MX", start=start, end=end)
+b = yf.download("AAPL", start=start, end=end)
+c = yf.download("TMO", start=start, end=end)
+d = yf.download("QCOM", start=start, end=end)
+e = yf.download("BAC", start=start, end=end)
+f = yf.download("WFC", start=start, end=end)
+g = yf.download("PFE", start=start, end=end)
+h = yf.download("MEGACPO.MX", start=start, end=end)
+i = yf.download("SKLZ", start=start, end=end)
+j = yf.download("CVX", start=start, end=end)
+k = yf.download("TGT", start=start, end=end)
+l = yf.download("NKE", start=start, end=end)
+m = yf.download("FANG", start=start, end=end)
+n = yf.download("SBUX", start=start, end=end)
+# Datos de las accionesart=start, end=end)
 
 # Concatenar las columnas
-stocks = pd.concat([ceva["Close"], google["Close"], tesla["Close"], zom["Close"]], axis=1)
+stocks = pd.concat([a["Close"]
+                    , b["Close"], c["Close"], d["Close"], e["Close"], f["Close"]
+                    , g["Close"], h["Close"], i["Close"], j["Close"], k["Close"]
+                    , l["Close"], m["Close"]
+                    , n["Close"]], axis=1)
 
 # Renombrar
-stocks.columns = ["CEVA", "GOOGLE", "TESLA", "ZOMEDICA"]
+stocks.columns = ["OXY1.MX", "AAPL", "TMO", "QCOM", "BAC", "WFC", "PFE", "MEGACPO.MX", "SKLZ", "CVX", "TGT", "NKE", "FANG", "SBUX"]
+
  
 print(stocks)
 
@@ -35,8 +49,8 @@ print(returns)
 #Logaritmos
 logReturns = np.log(returns)
 
-noDEPortafolios = 500
-weight = np.zeros((noDEPortafolios, 4))
+noDEPortafolios = 10000
+weight = np.zeros((noDEPortafolios, 14))
 ReturnEsp = np.zeros(noDEPortafolios)
 VolEsp = np.zeros(noDEPortafolios)
 RadioSharpe = np.zeros(noDEPortafolios)
@@ -45,7 +59,7 @@ meanlogReturns = logReturns.mean()
 Sigma = logReturns.cov()
 
 for k in range(noDEPortafolios):
-    w = np.array(np.random.random(4))
+    w = np.array(np.random.random(14))
     w = w / np.sum(w)
     weight[k, :] = w
     
@@ -58,6 +72,7 @@ for k in range(noDEPortafolios):
 
 maxIndex = RadioSharpe.argmax()
 weight[maxIndex, :]
+pesos = weight[maxIndex, :]
 
 # Grafica frontera eficiente
 plt.figure(figsize=(10, 6))
